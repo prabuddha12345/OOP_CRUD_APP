e4e5524775e99a9bf7f79260725d328c86ba3b83
Gajasingha P.B <prabuddhabhawana1@gmail.com>
2026-05-19 03:13:25 +0530

    implement endpoint to fetch active student details record

--- src/main/java/com/tutorease/controller/StudentController.java
+++ src/main/java/com/tutorease/controller/StudentController.java
@@ -2,4 +2,7 @@
 import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/students")
-public class StudentController {}
+public class StudentController {
+    @GetMapping("/{id}")
+    public String getStudent() { return "student"; }
+}
