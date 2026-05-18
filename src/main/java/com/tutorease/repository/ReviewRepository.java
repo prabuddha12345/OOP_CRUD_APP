c721e3e3797448821297d7addf226dde0cc0d0cd
Abhishek K.D.D <IT25101458@my.sliit.lk>
2026-05-19 03:13:14 +0530

    create review repository database query interface

---
+++ src/main/java/com/tutorease/repository/ReviewRepository.java
@@ -0,0 +1,4 @@
+﻿package com.tutorease.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tutorease.model.Review;
+public interface ReviewRepository extends JpaRepository<Review, Long> {}
