8f732efb3b599bb75aedaa395947a0eac968e76c
Ameera M.M.S.F <IT25103417@my.sliit.lk>
2026-05-19 03:13:18 +0530

    implement repository query method to find bookings by student id

--- src/main/java/com/tutorease/repository/BookingRepository.java
+++ src/main/java/com/tutorease/repository/BookingRepository.java
@@ -1,4 +1,7 @@
 ﻿package com.tutorease.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.tutorease.model.Booking;
-public interface BookingRepository extends JpaRepository<Booking, Long> {}
+import java.util.List;
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByStudentId(Long studentId);
+}
