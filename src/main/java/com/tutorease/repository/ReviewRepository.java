0e25c645246ea6168e5abfa2cf0b7e3ae6663741
Abhishek K.D.D <IT25101458@my.sliit.lk>
2026-05-19 03:13:14 +0530

    implement repository query method to calculate tutor average rating

--- src/main/java/com/tutorease/repository/ReviewRepository.java
+++ src/main/java/com/tutorease/repository/ReviewRepository.java
@@ -1,7 +1,12 @@
-﻿package com.tutorease.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+package com.tutorease.repository;
+
 import com.tutorease.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
+
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByTutorId(Long tutorId);
 }
