3c1a7829f025ff17dd9b8b6feac6dceeadcdb296
Jayaweera U.L.S.P <IT25102626@my.sliit.lk>
2026-05-19 03:13:22 +0530

    implement repository query method to find tutors by subject

--- src/main/java/com/tutorease/repository/TutorRepository.java
+++ src/main/java/com/tutorease/repository/TutorRepository.java
@@ -1,4 +1,7 @@
 ﻿package com.tutorease.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.tutorease.model.Tutor;
-public interface TutorRepository extends JpaRepository<Tutor, Long> {}
+import java.util.List;
+public interface TutorRepository extends JpaRepository<Tutor, Long> {
+    List<Tutor> findBySubject(String subject);
+}
