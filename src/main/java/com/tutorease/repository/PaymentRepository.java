ccb5895d7fc1716abe1d6ccdf9354b05697d0910
Damsana J.M.D.C <it25102694@my.sliit.lk>
2026-05-19 03:13:10 +0530

    implement repository query method to find payments by booking id

--- src/main/java/com/tutorease/repository/PaymentRepository.java
+++ src/main/java/com/tutorease/repository/PaymentRepository.java
@@ -1,4 +1,7 @@
 ﻿package com.tutorease.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.tutorease.model.Payment;
-public interface PaymentRepository extends JpaRepository<Payment, Long> {}
+import java.util.List;
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByBookingId(Long bookingId);
+}
