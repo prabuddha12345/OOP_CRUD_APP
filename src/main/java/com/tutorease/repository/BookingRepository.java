f38c6c9c40540cda6c251f5b455c2796f4da2bdf
Ameera M.M.S.F <IT25103417@my.sliit.lk>
2026-05-19 03:13:18 +0530

    create booking repository database query interface

---
+++ src/main/java/com/tutorease/repository/BookingRepository.java
@@ -0,0 +1,4 @@
+﻿package com.tutorease.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tutorease.model.Booking;
+public interface BookingRepository extends JpaRepository<Booking, Long> {}
