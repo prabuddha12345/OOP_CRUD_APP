5b0ec16f015da3ae6cd06200b330747fc34b2d15
Jayaweera U.L.S.P <IT25102626@my.sliit.lk>
2026-05-19 03:13:22 +0530

    create tutor repository database query interface

---
+++ src/main/java/com/tutorease/repository/TutorRepository.java
@@ -0,0 +1,4 @@
+﻿package com.tutorease.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tutorease.model.Tutor;
+public interface TutorRepository extends JpaRepository<Tutor, Long> {}
