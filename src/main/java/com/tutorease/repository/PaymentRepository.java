a32c78365ccaa981e2b3c5873f537f9ffe96ee6b
Damsana J.M.D.C <it25102694@my.sliit.lk>
2026-05-19 03:13:10 +0530

    create payment repository database query interface

---
+++ src/main/java/com/tutorease/repository/PaymentRepository.java
@@ -0,0 +1,4 @@
+﻿package com.tutorease.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tutorease.model.Payment;
+public interface PaymentRepository extends JpaRepository<Payment, Long> {}
