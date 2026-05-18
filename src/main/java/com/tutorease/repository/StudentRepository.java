e39f80206791267efe3c3a89675df9c6981ac071
Gajasingha P.B <prabuddhabhawana1@gmail.com>
2026-05-19 03:13:25 +0530

    create student repository database query interface

---
+++ src/main/java/com/tutorease/repository/StudentRepository.java
@@ -0,0 +1,4 @@
+﻿package com.tutorease.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tutorease.model.Student;
+public interface StudentRepository extends JpaRepository<Student, Long> {}
