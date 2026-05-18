eb2e8bea5e630eb472c44e2b9f7585b6f50e36f2
Abhishek K.D.D <IT25101458@my.sliit.lk>
2026-05-19 03:13:14 +0530

    implement repository query method to find reviews by tutor id

--- src/main/java/com/tutorease/repository/ReviewRepository.java
+++ src/main/java/com/tutorease/repository/ReviewRepository.java
@@ -1,4 +1,7 @@
 ﻿package com.tutorease.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.tutorease.model.Review;
-public interface ReviewRepository extends JpaRepository<Review, Long> {}
+import java.util.List;
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByTutorId(Long tutorId);
+}
