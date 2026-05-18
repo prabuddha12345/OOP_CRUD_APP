1ff6b3bc206231494f084ea9bbf8d4637b8016f4
Gajasingha P.B <prabuddhabhawana1@gmail.com>
2026-05-19 03:13:25 +0530

    implement repository query method to find student by email reference

--- src/main/java/com/tutorease/repository/StudentRepository.java
+++ src/main/java/com/tutorease/repository/StudentRepository.java
@@ -1,4 +1,9 @@
-﻿package com.tutorease.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+package com.tutorease.repository;
+
 import com.tutorease.model.Student;
-public interface StudentRepository extends JpaRepository<Student, Long> {}
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByUsername(String username);
+}
